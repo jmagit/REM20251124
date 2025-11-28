f53769465e8cb2cc376037cb7baf18a5f971bcf6
Javier <35864946+jmagit@users.noreply.github.com>
2025-11-28 12:25:13 +0100

    primer Benchmark

--- demo-jmh/src/main/java/com/example/Int2StringBenchmark.java
+++ demo-jmh/src/main/java/com/example/Int2StringBenchmark.java
@@ -1,17 +1,40 @@
 package com.example;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+@BenchmarkMode(Mode.Throughput)
+@Warmup(timeUnit = TimeUnit.SECONDS, time = 1, iterations = 2 )
+@Measurement(timeUnit = TimeUnit.SECONDS, time = 1, iterations = 5 )
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Fork(1)
 public class Int2StringBenchmark {
 
+	int caso = 10;
+	
     @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
+    public void baseline() {
+    }
+    
+    @Benchmark
+    public String IntegerToString() {
+    	return Integer.toString(caso);
+    }
+    
+    @Benchmark
+    public String StringValueOf() {
+    	return String.valueOf(caso);
     }
     
     public static void main(String[] args) throws RunnerException {
