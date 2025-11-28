ac91e188dbd6e68dcba2add3a95d883a7fe204e1
Javier <35864946+jmagit@users.noreply.github.com>
2025-11-28 12:49:57 +0100

    Int2StringBenchmark

--- demo-jmh/src/main/java/com/example/Int2StringBenchmark.java
+++ demo-jmh/src/main/java/com/example/Int2StringBenchmark.java
@@ -8,19 +8,24 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-@BenchmarkMode(Mode.Throughput)
+@BenchmarkMode(Mode.AverageTime)
 @Warmup(timeUnit = TimeUnit.SECONDS, time = 1, iterations = 2 )
 @Measurement(timeUnit = TimeUnit.SECONDS, time = 1, iterations = 5 )
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Fork(1)
+@State(Scope.Benchmark)
 public class Int2StringBenchmark {
 
+	@Param({ "10", "1234567890" })
 	int caso = 10;
 	
     @Benchmark
